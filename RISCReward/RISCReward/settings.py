5e45ef6d05672ef9c9516a15e0903b6147e29851
Yash-Keswani <yash20158@iiitd.ac.in>
2022-03-21 19:45:03 +0530

    updated requirements, added texttable and whitenoise, serving static files with debug off now
    Signed-off-by: Yash-Keswani <yash20158@iiitd.ac.in>

--- RISCReward/RISCReward/settings.py
+++ RISCReward/RISCReward/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v3ov$xw)^-=7uugy90!v@70690v4-5d=l0qhcii)d2-4$ah^i#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0"]
 
 # Application definition
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
 	'django.middleware.security.SecurityMiddleware',
+	'whitenoise.middleware.WhiteNoiseMiddleware',
 	'django.contrib.sessions.middleware.SessionMiddleware',
 	'django.middleware.common.CommonMiddleware',
 	'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,7 +117,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
 	BASE_DIR / "static",
 	]
-STATIC_ROOT = 'static/'
+STATICFILES_STORAGE = 'my_project.storage.WhiteNoiseStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
